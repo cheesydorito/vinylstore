93c107ee008f4a404e7b0bbf133f10d0b7cc05c5
cheesydorito <z_gignadze@cu.edu.ge>
2024-09-22 15:25:47 +0400

    task1

---
+++ src/Lib1.hs
@@ -0,0 +1,8 @@
+module Lib1
+    ( completions
+    ) where
+
+-- | This function returns a list of words
+-- to be autocompleted in your program's repl.
+completions :: [String]
+completions = ["store", "year", "genres", "genre", "singers", "singer", "vinyls", "vinyl"]
